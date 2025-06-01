54e2779294bc9814bbb2641737b21f1d80a77f17
guruprakash-c <c.guruprakash@yahoo.com>
2025-06-01 16:19:10 +0530

    Conditions & Looping

---
+++ src/routes/Conditions.svelte
@@ -0,0 +1,33 @@
+<script>
+let selectedOption = ''
+$: handleSelect = ((selectedOption != '') ? 'You have selected: Option #'+ selectedOption : 'Choose an option');
+
+let options = [
+    {
+        id: 1, text: 'Option #1'
+    },
+    {
+        id: 2, text: 'Option #2'
+    },
+    {
+        id: 3, text: 'Option #3'
+    },
+    {
+        id: 4, text: 'Option #4'
+    },
+    {
+        id: 5, text: 'Option #5'
+    }
+];
+</script>
+
+<select 
+    bind:value={selectedOption} 
+    on:change={handleSelect}
+>
+    <option value="">-- Select --</option>
+    {#each options as option}
+        <option value={option.id}>{option.text}</option>
+    {/each}
+</select>
+<p>{handleSelect}</p>
