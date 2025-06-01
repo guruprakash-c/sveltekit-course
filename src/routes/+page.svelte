aa3954c92dfc941570977ab041a9f18e663adba5
guruprakash-c <c.guruprakash@yahoo.com>
2025-06-01 15:49:15 +0530

    Form Fields : Age Calculator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script>
-import FirstComponent from './FirstComponent.svelte';
+//import FirstComponent from './FirstComponent.svelte';
+import FormFields from './FormFields.svelte';
 </script>
-<FirstComponent src={'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzl0Ym11NWRoa3V2dWF6aTZmbWdzNXFuMWs3ejJtYW94YmN1NXA3aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VDMHgvx8N9U7UUlczQ/giphy.gif'} name={'running'}/>
+<!-- FirstComponent src={'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzl0Ym11NWRoa3V2dWF6aTZmbWdzNXFuMWs3ejJtYW94YmN1NXA3aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VDMHgvx8N9U7UUlczQ/giphy.gif'} name={'running'}/ -->
+
+<FormFields />
---
+++ src/routes/FormFields.svelte
@@ -0,0 +1,8 @@
+<script>
+let today = (new Date()).getFullYear();
+let dob = 0;
+$: calculatedAge = (today - (new Date(dob)).getFullYear());
+</script>
+
+<input type="date" bind:value={dob} />
+<p>You age is: {calculatedAge}</p>
